1a338a1cc77829fdd3033e32e928445b145cf117
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2017-07-04 18:15:40 +0200

    Changed while-loop into for-loop in turnIntoContactBook method

--- src/main/java/contactbook/logic/controller/ContactBookDeserialiser.java
+++ src/main/java/contactbook/logic/controller/ContactBookDeserialiser.java
@@ -10,14 +10,12 @@ class ContactBookDeserialiser {
     List<Contact> turnIntoContactBook(List<String> inputInStrings) {
         ContactDeserialiser deserialiser = new ContactDeserialiser();
         int stringsInOneContact = 4;
-        int amountOfStrings = inputInStrings.size();
-        int amountOfIterations = amountOfStrings / stringsInOneContact;
         List<Contact> contactBook = new ArrayList<>();
-        int startOfContact = -4;
-        while (amountOfIterations > 0) {
-            contactBook.add(deserialiser.turnIntoContact(inputInStrings, startOfContact + 4));
-            startOfContact += 4;
-            amountOfIterations--;
+
+        for (int startOfContact = 0;
+             startOfContact <= inputInStrings.size() - stringsInOneContact;
+             startOfContact += stringsInOneContact) {
+            contactBook.add(deserialiser.turnIntoContact(inputInStrings, startOfContact + stringsInOneContact));
         }
         return contactBook;
     }
