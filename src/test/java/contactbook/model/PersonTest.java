564752a097c6ff0bbf1d7f5b720a7d4f4cd4bf0a
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2017-07-10 14:24:01 +0200

    Added two tests to PersonTest class

--- src/test/java/contactbook/model/PersonTest.java
+++ src/test/java/contactbook/model/PersonTest.java
@@ -5,12 +5,23 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PersonTest {
-
-    private Person person = new Person("Ray", "Bradbury");
+    private String firstName = "Ray";
+    private String lastName = "Bradbury";
+    private Person person = new Person(firstName, lastName);
 
     @Test
     public void shouldProduceProperString(){
         String expected = "First name: Ray\nLast name: Bradbury\n";
         assertEquals(expected, person.toString());
+    }
+
+    @Test
+    public void shouldReturnFirstName(){
+        assertEquals(firstName, person.getFirstName());
+    }
+
+    @Test
+    public void shouldReturnLastName(){
+        assertEquals(lastName, person.getLastName());
     }
 }
