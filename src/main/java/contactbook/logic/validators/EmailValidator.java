9998908b3edfd6b4cd6202b0df12d37176b0af9a
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2017-07-11 19:51:45 +0200

    Changed regex for e-mail validation; improved tests in EmailValidatorTest class

--- src/main/java/contactbook/logic/validators/EmailValidator.java
+++ src/main/java/contactbook/logic/validators/EmailValidator.java
@@ -5,8 +5,10 @@ public class EmailValidator implements Validator {
             "Your e-mail contains improper characters! Please, enter proper e-mail";
 
     public boolean isValid(String input) {
-        String regex = "[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*" +
-                "[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+//        String regex = "[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*" +
+//                "[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+        String regex = "[-\\w]+(\\.[-\\w]+)*@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+";
+
         return input.matches(regex);
     }
 }
--- src/test/java/contactbook/logic/validators/EmailValidatorTest.java
+++ src/test/java/contactbook/logic/validators/EmailValidatorTest.java
@@ -6,23 +6,23 @@ import static org.junit.Assert.*;
 
 public class EmailValidatorTest {
 
+    private EmailValidator validator = new EmailValidator();
+
     @Test
     public void testProperEMail(){
         String eMail = "asterisk0123456789@gmail.com";
         String eMail1 = "k.smith@letters.no";
         String eMail2 = "mother_tereza@not.here";
         String eMail3 = "Mike@hot.there";
-//        String eMail4 = "not/I@i.ua";
-//        String eMail5 = "!#$%&'*+/=?^_`{|}~-@yandex.ru";
-
-        EmailValidator validator = new EmailValidator();
+        String eMail4 = "not-I@i.ua";
+        String eMail5 = "eric.monte-carlo@Adjos55.amigo3";
 
         assertTrue(validator.isValid(eMail));
         assertTrue(validator.isValid(eMail1));
         assertTrue(validator.isValid(eMail2));
         assertTrue(validator.isValid(eMail3));
-//        assertTrue(validator.isValid(eMail4));
-//        assertTrue(validator.isValid(eMail5));
+        assertTrue(validator.isValid(eMail4));
+        assertTrue(validator.isValid(eMail5));
     }
 
     @Test
@@ -31,8 +31,6 @@ public class EmailValidatorTest {
         String eMail1 = "@mama@gmail.com";
         String eMail2 = "sister@gmail@.com";
         String eMail3 = "tree@@@monkey.com";
-
-        EmailValidator validator = new EmailValidator();
 
         assertFalse(validator.isValid(eMail));
         assertFalse(validator.isValid(eMail1));
@@ -47,14 +45,13 @@ public class EmailValidatorTest {
         String eMail2 = "Tim.Roth@big,stars";
         String eMail3 = "Tim.Roth@big:stars";
         String eMail4 = "Tim;Roth@big;stars";
-
-        EmailValidator validator = new EmailValidator();
+        String eMail5 = "!#$%&'*+/=?^`{|}~@yandex.ru";
 
         assertFalse(validator.isValid(eMail));
         assertFalse(validator.isValid(eMail1));
         assertFalse(validator.isValid(eMail2));
         assertFalse(validator.isValid(eMail3));
         assertFalse(validator.isValid(eMail4));
+        assertFalse(validator.isValid(eMail5));
     }
-
 }
